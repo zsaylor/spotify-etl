2998aaabaa52c8dc8b50567ff7fecdb6aef3a644
Zachary Saylor <zchrysaylor@gmail.com>
2021-03-31 16:55:47 +0200

    Completed Part 3: Load. Used SqlAlchemy & SQLite to create a table & push the dataframe to the database.

--- main.py
+++ main.py
@@ -9,37 +9,40 @@ import sqlite3
 
 
 DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
-USER_ID = "zchrysaylor"
-TOKEN = "BQBUK-WVG6aUVsNtZuSn45kpLUTY4S-OT1hV1UqhFoIQIW2rdIWpTVRELkDkBg26JGtTsZQbCTYxM5XlsIlRwDozaeU4ivmiS2K2wzLm4YXFQCPxorhu0aoa7b130nQWzJCef6n3ct_hgOvnoY458g"
+USER_ID = "xe5yGtYvRwOpNBsJnSRQcg"
+TOKEN = "BQAuasQehCsutypvVa3mck160284dlgkiQR9qhxxqpn2W4U7kpoUZfEi11JJEZBpvdJATkGgIEODXAxNKONoQcJ0a52pMgPGNszBEHKYPbRs9kOZdoUC1y1IT6ycwOlwp5e0LXL-epZ-OEYdzN8kkA"
 
 # Note: Token expires every 15 minutes.
 # Generate new token here: https://developer.spotify.com/console/get-recently-played/
+
 
 def check_if_valid_data(df: pd.DataFrame) -> bool:
     # Check if DataFrame is empty
     if df.empty:
         print("No songs downloaded. Finishing execution.")
         return False
-    
+
     # Primary Key check
-    if pd.Series(df['played_at']).is_unique:
+    if pd.Series(df["played_at"]).is_unique:
         pass
     else:
         raise Exception("Primary Key check is violated.")
-    
+
     # Check for null values
     if df.isnull().values.any():
         raise Exception("Null value found.")
-    
+
     # Check that all timestamps are from last 24 hours
     yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
     yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
 
     timestamps = df["timestamp"].tolist()
     for timestamp in timestamps:
-        datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
-        raise Exception("At least 1 returned song was not played in the last 24 hours.")
-    
+        if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
+            raise Exception(
+                "At least 1 returned song was not played in the last 24 hours."
+            )
+
     return True
 
 
@@ -88,7 +91,32 @@ if __name__ == "__main__":
 
     print(song_df)
 
-
     # Validate
-    if(check_if_valid_data(song_df)):
+    if check_if_valid_data(song_df):
         print("Data valid, proceeding to Load stage...")
+
+    # Load
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect("my_played_tracks.sqlite")
+    cursor = conn.cursor()
+
+    sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+
+    cursor.execute(sql_query)
+    print("Database succesfully opened...")
+
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists="append")
+    except:
+        print("Data already in database...")
+
+    conn.close()
+    print("Database successfully closed...")
